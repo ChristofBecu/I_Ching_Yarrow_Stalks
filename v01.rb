e78de4653645892a62f6793d75c3ed02c8b975c2
Christof Becu <christof.becu@gmail.com>
2018-06-19 15:07:32 +0200

    a lot of changes

--- v01.rb
+++ v01.rb
@@ -6,55 +6,136 @@
 # Like this the program doesn't need a single loop to yield a line and is so much easier to read.
 
 # probability values of the yarrow stalks method
-$Probability = Hash.new
-$Probability[:six]   = 0.0625 # 1/16 =  6.25% = old yin
-$Probability[:nine]  = 0.1875 # 3/16 = 18.75% = old yang
-$Probability[:seven] = 0.3125 # 5/16 = 31.25% = young yang
-$Probability[:eight] = 0.4375 # 7/16 = 43.75% = young yin
+module Probability
+  Probability = Hash.new
+  Probability[:six]   = 0.0625 # 1/16 =  6.25% = old yin  === x ===
+  Probability[:nine]  = 0.1875 # 3/16 = 18.75% = old yang ====o====
+  Probability[:seven] = 0.3125 # 5/16 = 31.25% = young yang ========= 
+  Probability[:eight] = 0.4375 # 7/16 = 43.75% = young yin ===   ===
+end
 
-# binary values of trigrams
-$Trigram = Hash.new
-$Trigram["000"] = "K'oen - Earth"
-$Trigram["001"] = "Ken - Mountain" 
-$Trigram["010"] = "K'an - Water"
-$Trigram["011"] = "Soen - Wind"
-$Trigram["100"] = "Tsjen - Thunder" 
-$Trigram["101"] = "Li - Fire"
-$Trigram["110"] = "Twèi - Lake"
-$Trigram["111"] = "Tj'ièn - Heaven"
+module InfoTrigram
+  # binary values of trigrams
+  Trigram = Hash.new
+  Trigram["000"] = "K'oen - Earth"
+  Trigram["001"] = "Ken - Mountain" 
+  Trigram["010"] = "K'an - Water"
+  Trigram["011"] = "Soen - Wind"
+  Trigram["100"] = "Tsjen - Thunder" 
+  Trigram["101"] = "Li - Fire"
+  Trigram["110"] = "Twèi - Lake"
+  Trigram["111"] = "Tj'ièn - Heaven"
+end
+
+module InfoHexagram
+  Hexagram = Hash.new
+  Hexagram["000000"] = "2"
+  Hexagram["000001"] = "23"
+  Hexagram["000010"] = "8"
+  Hexagram["000011"] = "20"
+  Hexagram["000100"] = "16"
+  Hexagram["000101"] = "35"
+  Hexagram["000110"] = "45"
+  Hexagram["000111"] = "12"
+  Hexagram["001000"] = "15"
+  Hexagram["001001"] = "52"
+  Hexagram["001010"] = "39"
+  Hexagram["001011"] = ""
+  Hexagram["001100"] = ""
+  Hexagram["001101"] = ""
+  Hexagram["001110"] = ""
+  Hexagram["001111"] = ""
+  Hexagram["010000"] = ""
+  Hexagram["010001"] = ""
+  Hexagram["010010"] = ""
+  Hexagram["010011"] = ""
+  Hexagram["010100"] = ""
+  Hexagram["010101"] = ""
+  Hexagram["010110"] = ""
+  Hexagram["010111"] = ""
+  Hexagram["011000"] = ""
+  Hexagram["011001"] = ""
+  Hexagram["011010"] = ""
+  Hexagram["011011"] = ""  
+  Hexagram["011100"] = ""
+  Hexagram["011101"] = ""
+  Hexagram["011110"] = ""
+  Hexagram["011111"] = ""
+  Hexagram["100000"] = ""
+  Hexagram["100001"] = ""
+  Hexagram["100010"] = ""
+  Hexagram["100011"] = ""
+  Hexagram["100100"] = ""
+  Hexagram["100101"] = ""
+  Hexagram["100110"] = ""
+  Hexagram["100111"] = ""
+  Hexagram["101000"] = ""
+  Hexagram["101001"] = ""
+  Hexagram["101010"] = ""
+  Hexagram["101011"] = ""
+  Hexagram["101100"] = ""
+  Hexagram["101101"] = ""
+  Hexagram["101110"] = ""
+  Hexagram["101111"] = ""
+  Hexagram["110000"] = ""
+  Hexagram["110001"] = ""
+  Hexagram["110010"] = ""
+  Hexagram["110011"] = ""
+  Hexagram["110100"] = ""
+  Hexagram["110101"] = ""
+  Hexagram["110110"] = ""
+  Hexagram["110111"] = ""
+  Hexagram["111000"] = ""
+  Hexagram["111001"] = ""
+  Hexagram["111010"] = ""
+  Hexagram["111011"] = ""
+  Hexagram["111100"] = ""
+  Hexagram["111101"] = ""
+  Hexagram["111110"] = ""
+  Hexagram["111111"] = ""
+end
 
 class Hexagram
-
-  def initialize
+  include Probability
+  include InfoTrigram
+  include InfoHexagram
+  
+  def initialize(lines=[castLine, castLine, castLine, castLine, castLine, castLine])
     # Cast a new line for each hexagram[key]
     @hexagram = Hash.new
-    @hexagram[:"bottom line"] = castLine
-    @hexagram[:"second line"] = castLine
-    @hexagram[:"third line"]  = castLine
-    @hexagram[:"fourth line"] = castLine
-    @hexagram[:"fifth line"]  = castLine
-    @hexagram[:"top line"]    = castLine
+    @hexagram[:"top line"]    = lines[0]
+    @hexagram[:"fifth line"]  = lines[1] 
+    @hexagram[:"fourth line"] = lines[2]
+    @hexagram[:"third line"]  = lines[3]
+    @hexagram[:"second line"] = lines[4]
+    @hexagram[:"bottom line"] = lines[5]
+    Hash[@hexagram.to_a.reverse]
+
+   
     
     # Determine if hexagram[key] is a moving line
     @movinglines = Hash.new
-    @movinglines[:"bottom line"]  = @hexagram[:"bottom line"] == :"old yin"  ? true : @hexagram[:"bottom line"] == :"old yang"  ? true : false
-    @movinglines[:"second line"]  = @hexagram[:"second line"] == :"old yin"  ? true : @hexagram[:"second line"] == :"old yang"  ? true : false
-    @movinglines[:"third line"]   = @hexagram[:"third line"]  == :"old yin"  ? true : @hexagram[:"third line"]  == :"old yang"  ? true : false
-    @movinglines[:"fourth line"]  = @hexagram[:"fourth line"] == :"old yin"  ? true : @hexagram[:"fourth line"] == :"old yang"  ? true : false
-    @movinglines[:"fifth line"]   = @hexagram[:"fifth line"]  == :"old yin"  ? true : @hexagram[:"fifth line"]  == :"old yang"  ? true : false
-    @movinglines[:"top line"]     = @hexagram[:"top line"]    == :"old yin"  ? true : @hexagram[:"top line"]    == :"old yang"  ? true : false
+    @movinglines[:"bottom line"]  = @hexagram[:"bottom line"] == 6  ? true : @hexagram[:"bottom line"] == 9  ? true : false
+    @movinglines[:"second line"]  = @hexagram[:"second line"] == 6  ? true : @hexagram[:"second line"] == 9  ? true : false
+    @movinglines[:"third line"]   = @hexagram[:"third line"]  == 6  ? true : @hexagram[:"third line"]  == 9  ? true : false
+    @movinglines[:"fourth line"]  = @hexagram[:"fourth line"] == 6  ? true : @hexagram[:"fourth line"] == 9  ? true : false
+    @movinglines[:"fifth line"]   = @hexagram[:"fifth line"]  == 6  ? true : @hexagram[:"fifth line"]  == 9  ? true : false
+    @movinglines[:"top line"]     = @hexagram[:"top line"]    == 6  ? true : @hexagram[:"top line"]    == 9  ? true : false
     
     # Solid line (Yang) = 1; broken line (Yin) = 0
     @binary = Hash.new
-    @binary[:"bottom line"]  = @hexagram[:"bottom line"] == :"old yin" ? 0 :  @hexagram[:"bottom line"] == :"young yin" ? 0 : 1
-    @binary[:"second line"]  = @hexagram[:"second line"] == :"old yin" ? 0 :  @hexagram[:"second line"] == :"young yin" ? 0 : 1
-    @binary[:"third line"]   = @hexagram[:"third line"]  == :"old yin" ? 0 :  @hexagram[:"third line"]  == :"young yin" ? 0 : 1
-    @binary[:"fourth line"]  = @hexagram[:"fourth line"] == :"old yin" ? 0 :  @hexagram[:"fourth line"] == :"young yin" ? 0 : 1
-    @binary[:"fifth line"]   = @hexagram[:"fifth line"]  == :"old yin" ? 0 :  @hexagram[:"fifth line"]  == :"young yin" ? 0 : 1
-    @binary[:"top line"]     = @hexagram[:"top line"]    == :"old yin" ? 0 :  @hexagram[:"top line"]    == :"young yin" ? 0 : 1
+    @binary[:"bottom line"]  = @hexagram[:"bottom line"] == 6 ? 0 :  @hexagram[:"bottom line"] == 8 ? 0 : 1
+    @binary[:"second line"]  = @hexagram[:"second line"] == 6 ? 0 :  @hexagram[:"second line"] == 8 ? 0 : 1
+    @binary[:"third line"]   = @hexagram[:"third line"]  == 6 ? 0 :  @hexagram[:"third line"]  == 8 ? 0 : 1
+    @binary[:"fourth line"]  = @hexagram[:"fourth line"] == 6 ? 0 :  @hexagram[:"fourth line"] == 8 ? 0 : 1
+    @binary[:"fifth line"]   = @hexagram[:"fifth line"]  == 6 ? 0 :  @hexagram[:"fifth line"]  == 8 ? 0 : 1
+    @binary[:"top line"]     = @hexagram[:"top line"]    == 6 ? 0 :  @hexagram[:"top line"]    == 8 ? 0 : 1
     
-    @lowerTrigram = $Trigram[@binary.values.to_s.tr("[ ,]", "")[0..2]]
-    @upperTrigram = $Trigram[@binary.values.to_s.tr("[ ,]", "")[3..5]]
+    @lowerTrigram = Trigram[@binary.values.to_s.tr("[ ,]", "")[0..2]]
+    @upperTrigram = Trigram[@binary.values.to_s.tr("[ ,]", "")[3..5]]
+    
+    @nameHexagram = Hexagram[@binary.values.to_s.tr("[ ,]", "")]
+    
   end
   
   def castLine
@@ -64,14 +145,14 @@ class Hexagram
     # of the yarrow stalks to generate a line accordingly
     cast = rand 
     case 
-    when cast < $Probability[:six]
-      :"old yin"
-    when cast < $Probability[:six] + $Probability[:nine]
-      :"old yang"
-    when cast < $Probability[:six] + $Probability[:nine] + $Probability[:seven]
-      :"young yang"
-    when cast < $Probability[:six] + $Probability[:nine] + $Probability[:seven] + $Probability[:eight]
-      :"young yin"
+    when cast < Probability[:six]
+      6
+    when cast < Probability[:six] + Probability[:nine]
+      9
+    when cast < Probability[:six] + Probability[:nine] + Probability[:seven]
+      7
+    when cast < Probability[:six] + Probability[:nine] + Probability[:seven] + Probability[:eight]
+      8
     end
   end
   
@@ -79,18 +160,22 @@ class Hexagram
     #convert hash to array, reverse, and back to hash
     Hash[@hexagram.to_a.reverse].each do |key, value|
       case value
-      when :"old yin"
-        puts "=== x ===   |   ===   ===   >   ========="
-      when :"young yin"
-        puts "===   ===   |   ===   ===   >   ===   ==="
-      when :"old yang"
-        puts "====o====   |   =========   >   ===   ==="
-      when :"young yang"
-        puts "=========   |   =========   >   ========="
+      when 6
+        puts "=== x ==="
+      when 8
+        puts "===   ==="
+      when 9
+        puts "====o===="
+      when 7
+        puts "========= "
       end
     end
   end
   
+  def movingLines?
+    @movinglines.values.include?(true) 
+  end
+
   def printMovingLines
     if @movinglines.values.include?(true) 
       print "\nmoving lines:\n"
@@ -106,6 +191,11 @@ def printTrigrams
   puts
   print "Upper Trigram = ", @upperTrigram, "\n"
   print "Lower Trigram = ", @lowerTrigram, "\n"
+  print "Hexagram = ", @nameHexagram, "\n"
+end
+
+def returnHexagram
+  return @hexagram
 end
 
 end # of Class Hexagram
@@ -113,11 +203,33 @@ end # of Class Hexagram
 hex = Hexagram.new
 hex.drawHexagram
 hex.printTrigrams
-hex.printMovingLines
 
-h = Hexagram.new
-h.drawHexagram
-h.printTrigrams
-h.printMovingLines
 
-# end
+h = hex.returnHexagram
+
+puts "\n   evolves to:\n\n"
+
+while hex.movingLines?
+  h.each do |key, value|
+  
+    if value == 6 
+      h[key] = 7
+      break
+    end
+    if value == 9
+      h[key] = 8
+      break
+    end
+  end
+  e = Hexagram.new(h.values.to_a)
+
+  e.drawHexagram
+  e.printTrigrams
+  if !e.movingLines?
+    break
+  end
+  puts "\n   evolves to:\n\n" 
+
+end
+
+
